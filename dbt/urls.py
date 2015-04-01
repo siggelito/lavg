cea59b89961fcfba219e9503c53b408f0da2e732
Wahjlen <Wahjlen@10.12.10.157>
2015-04-01 11:38:03 +0200

    ändrat startsida

--- dbt/urls.py
+++ dbt/urls.py
@@ -5,5 +5,5 @@ from polls import views
 urlpatterns = patterns('',
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.index, name='index'),
+    url(r'', views.index, name='index'),
 )
