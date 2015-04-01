0e17954df0d2fcd2d192781ba38daedff6359d2a
Wahjlen <Wahjlen@10.12.10.157>
2015-04-01 11:32:25 +0200

    ändra url

--- dbt/urls.py
+++ dbt/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from polls import views
 
 urlpatterns = patterns('',
     url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index, name='index'),
 )
