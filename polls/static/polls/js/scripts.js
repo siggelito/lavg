90ad2a9290e4345cdf0ab39429c26d640c25c8a9
Mattias <mattias.edin92@gmail.com>
2015-04-14 08:55:53 +0200

    new upload

--- polls/static/polls/js/scripts.js
+++ polls/static/polls/js/scripts.js
@@ -16,8 +16,25 @@ function readURL(input) {
 }
     
 $("#imgInp1").change(function(){
+    alert("hej");
     readURL(this);
-    });
+});
 $("#imgInp2").change(function(){
     readURL(this);
 });
+
+function upload_img(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            
+            alert("hej" + video.length);
+        }
+        alert("hej" + video.length);
+        reader.readAsDataURL(input.files[0]);
+        video[video.length] = input.files[0];
+        alert("hej" + video.length);
+        &("#counterText").text(video.length);
+    }
+}
