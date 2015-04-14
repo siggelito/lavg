3f97109b22b68e5023353d35181846fc3f8ea1ec
unknown <sandraingerhage@gmail.com>
2015-04-14 09:18:20 +0200

    meddelande

--- polls/static/polls/js/scripts.js
+++ polls/static/polls/js/scripts.js
@@ -27,11 +27,11 @@ function upload_img(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
 
-        reader.onload = function (e) {
+        /*reader.onload = function (e) {
             
             alert("hej" + video.length);
-        }
-        alert("hej" + video.length);
+        }*/
+        //alert("hej" + video.length);
         reader.readAsDataURL(input.files[0]);
         video[video.length] = input.files[0];
         alert("hej" + video.length);
