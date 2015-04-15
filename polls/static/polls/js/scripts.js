28961c1eeff31f637bc4d06fbcbbdf159c948d26
Mattias <mattias.edin92@gmail.com>
2015-04-15 14:06:21 +0200

    Upload images and show

--- polls/static/polls/js/scripts.js
+++ polls/static/polls/js/scripts.js
@@ -9,16 +9,44 @@ $(document).ready(function(){
         var img = new Image;
         img.src = URL.createObjectURL(e.target.files[0]);
 
-        /* To render the image in canvas do: */
-        var ctx = document.getElementById('canvas').getContext('2d');
-        img.onload = function() {
-            ctx.drawImage(img, 20,20);
-            alert('the image is drawn');
-        }
-        /*end*/
+        for (var i = e.target.files.length - 1; i >= 0; i--) {
+            var img = new Image;
+            img.src = URL.createObjectURL(e.target.files[i]);
+            video[video.length] = img;
+        };
+        alert("nr of files: " + video.length);
 
-        /* Append image to video array */
-        video[video.length] = img;
-        alert(video.length);
+        var current = 0;
+        var canvas = document.getElementById('slideshow');
+        var context = canvas.getContext('2d');
+        
+        setTimeout(function(){
+
+            $('#slideshow').click(function(){
+                var nextIndex    = 0;
+                
+                if(video.length-1 > current){
+                    nextIndex = current +1;
+                }
+                else {
+                    nextIndex = 0;
+                }
+                var next = video[nextIndex];
+
+                // This browser supports canvas, fade it into view:
+
+                var imageRatio = ( next.width / next.height );
+                var imageScaledWidth = ( imageRatio * canvas.height );
+                // Show the next slide below the current one:
+                context.drawImage(next, ( (canvas.width - imageScaledWidth) / 2 ), 0, imageScaledWidth, canvas.height );
+                //next.show();
+                current = nextIndex;
+            });
+
+        },100);
     }
 }) 
+
+
+
+
