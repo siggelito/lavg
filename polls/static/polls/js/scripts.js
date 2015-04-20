64900d1a140859afebcf469d74c810333955b01c
Mattias <mattias.edin92@gmail.com>
2015-04-20 10:25:17 +0200

    new rendering with canvas

--- polls/static/polls/js/scripts.js
+++ polls/static/polls/js/scripts.js
@@ -1,58 +1,56 @@
-$(document).ready(function(){ 
-    var video = new Array();
 
+$(document).ready(function(){ 
+    var video = [];
+    var slideshow = document.getElementById('slideshow');
     var input = document.getElementById('input');
     input.addEventListener('change', handleFiles);
 
     function handleFiles(e) {
-        
-        var img = new Image;
-        img.src = URL.createObjectURL(e.target.files[0]);
+
+        var sources = [];
 
         for (var i = e.target.files.length - 1; i >= 0; i--) {
-            var img = new Image;
-            img.src = URL.createObjectURL(e.target.files[i]);
-            video[video.length] = img;
-        };
-        alert("nr of files: " + video.length);
+            
+            sources[i] = URL.createObjectURL(e.target.files[i]);
 
-        var current = 0;
-        var nextIndex = 0;
-        var canvas = document.getElementById('slideshow');
-        var context = canvas.getContext('2d');
-        var isRunning = false;
-        
-        setTimeout(function(){
+            content = new Object();
+            content.canvas = document.createElement('canvas');
+            video[video.length] = content;
 
-            $(canvas).click(function(){
+            var element = document.createElement("li");
 
-                if(video.length-1 > current){
-                    nextIndex = current +1;
-                }
-                else {
-                    nextIndex = 0;
-                }
-                var next = video[nextIndex];
+            element.appendChild(content.canvas);
+            slideshow.appendChild(element);
+        }
 
-                $(canvas).fadeOut(800,function() {
+        loadImages(sources, function(images) {
+            for (var i = 0; i <= images.length - 1; i++) {
+                
+                video[i].image = images[i];
 
-                    var size = calcSize(canvas, next);
-                    var width = size[0];
-                    var height = size[1];
+                var canvas = video[i].canvas;
+                var image = video[i].image;
 
-                    context.drawImage(next, ( (canvas.width - width) / 2 ), ( (canvas.height - height) / 2 ), width, height );
-                    $(canvas).fadeIn(400);
-                    current = nextIndex;
-                });
-            });
+                var size = calcSize(canvas, image);
+                var width = size[0];
+                var height = size[1];
 
-        },100);
+                var context = canvas.getContext('2d');
+                context.drawImage(image, ( (canvas.width - width) / 2 ), ( (canvas.height - height) / 2 ), width, height );  
+            };
+            video = images;
+        });
+
+        slideshow = $("#slideshow li");
     }
 }) 
 
 function calcSize( canvas, image ) {
     var imageRatio = ( image.width / image.height );
-
+    var withcanv = canvas.width;
+    var withimg = image.width;
+    var heightcanv = canvas.height;
+    var heigthimg = image.height;
     var widthScalingFactor = ( imageRatio * canvas.height ) / image.width;
     var heightScalingFactor = ( canvas.width / imageRatio ) / image.height;
 
@@ -67,8 +65,23 @@ function calcSize( canvas, image ) {
     return [width, height];
 }
 
-
-
-
+function loadImages(sources, callback) {
+    var images = [];
+    var loadedImages = 0;
+    var numImages = 0;
+    // get num of sources
+    for(var src in sources) {
+        numImages++;
+    }
+    for(var src in sources) {
+        images[src] = new Image();
+        images[src].onload = function() {
+            if(++loadedImages >= numImages) {
+                callback(images);
+            }
+        };
+        images[src].src = sources[src];
+    }
+}
 
 
