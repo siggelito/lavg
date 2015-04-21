009bb671c4fd082fccc512453df8cc7a75cbdfc5
wahjlen <micke_wahlstrom@hotmail.com>
2015-04-21 15:03:26 +0200

    beginning on image settings.

--- dbtapp/static/dbtapp/js/showimages.js
+++ dbtapp/static/dbtapp/js/showimages.js
@@ -2,12 +2,20 @@ function showImages(imageObject, index) {
 	var nrOfPics = $('#images li').length;
 	var imageUl = document.getElementById('images');
 	var image = imageObject.image;
-	image.width = 200;
-	image.height = 200;
+	image.width = 200; //333
+	image.height = 200; //eller 120
 	
 	if(index >= nrOfPics) {
 		var element = document.createElement('li');
 		element.appendChild(image);
 		imageUl.appendChild(element);
 	}
+}
+
+function openImageSetting() {
+	var allImages = document.getElementById("images");
+	var images = allImages.getElementsByTagName("li");
+	for (image in images) {
+		alert("Tryckt på bild!");
+	}	
 }
