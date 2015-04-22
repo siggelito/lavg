86c3c36b20bb1b0a6bb606b9f31667e599b47588
Mattias <mattias.edin92@gmail.com>
2015-04-22 18:33:26 +0200

    cleaned upp some code

--- dbtapp/static/dbtapp/js/preview.js
+++ dbtapp/static/dbtapp/js/preview.js
@@ -17,36 +17,28 @@ function initPreview (imageSet) {
 }
 
 function runSlideShow(imageSet, current) {
-
-
     if (current == 0) {
     	startAnimation(imageSet[0]);
     	var logo = document.getElementById('logo-image');
     	if (logo != null) {
     		$(logo).show();
     		logo.style.zIndex = 99999;
-
-    		if (logo.width > 51) {
+    		/*
+    		if ((logo.width > 51) || (logo < 48)) {
     			var imageRatio = ( logo.width / logo.height );
 		    	var scalingFactor = ( imageRatio * 50 ) / logo.width;
 		    	logo.width = logo.width * scalingFactor; 
-    		};
-	    	
-    	};
-    	       
-
+    		};*/
+    	}; 
     } else {
     	imageSet[current].settings.transition(imageSet[current-1], imageSet[current]);        
     }
     current++;
-
     if (current < imageSet.length) {
         setTimeout(function(){runSlideShow(imageSet, current)}, imageSet[current].settings.duration);
     } else {
         setTimeout(function() {
         	endAnimation(imageSet[current-1]);
         }, imageSet[current-1].settings.duration);
-        
     }
-    
 }
