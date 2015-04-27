5128558f335154b96e4a11aec07ce9668a4a5a58
wahjlen <micke_wahlstrom@hotmail.com>
2015-04-27 11:15:31 +0200

    Bug fix: preview timer nulled

--- dbtapp/static/dbtapp/js/preview.js
+++ dbtapp/static/dbtapp/js/preview.js
@@ -1,51 +1,42 @@
 
 function initPreview (imageSet) {	
-	/* Oanvänd funktion!
-	var startButton = document.getElementById("start-button");
-	
-    $(startButton).on("click", function() {
-		imageSet = sortSlideShow(imageSet, 0);
-    	runSlideShow(imageSet, 0);
-    });
-	*/
-
     $("#preview-button").delay(1000).on("click", function() {
 		imageSet = sortSlideShow(imageSet);
     	runSlideShow(imageSet, 0);
     });
 }
 
-function runSlideShow(imageSet, current) {
+var timer = null;
 
-    if (current == 0) {
-    	startAnimation(imageSet[0]);
-    	var logo = document.getElementById('logo-image');
-    	if (logo != null) {
-    		$(logo).show();
-    		logo.style.zIndex = 99999;
-    		
-			//if ((logo.width > 51) || (logo < 48)) {
-			if (logo.width > 70) {
-    			var imageRatio = ( logo.width / logo.height );
-		    	var scalingFactor = ( imageRatio * 50 ) / logo.width;
-		    	logo.width = logo.width * scalingFactor; 
-    		};
-    	}; 
-    } else {
-    	imageSet[current].settings.transition(imageSet[current-1], imageSet[current]);        
-    }
-    current++;
-    if (current < imageSet.length) {
-        timer = setTimeout(function(){runSlideShow(imageSet, current)}, imageSet[current].settings.duration);
-    } else {
-        timer = setTimeout(function() {
-        	endAnimation(imageSet[current-1]);
-        }, imageSet[current-1].settings.duration);
-    }
+function runSlideShow(imageSet, current) {
 	
 	$(".close").click(function() {
-		clearTimeout(timer);
 		$(imageSet[current].layers).hide();
-        //$(imageSet[current].layers).hide();
+		clearTimeout(timer);
+		timer = null;
 	});
+	
+	if (current == 0) {
+		startAnimation(imageSet[0]);
+	} else {
+		imageSet[current].settings.transition(imageSet[current-1], imageSet[current]);        
+	}
+	current++;
+	
+	if (current < imageSet.length) {
+		if(timer){
+			clearTimeout(timer);
+			timer = null;
+		}
+		timer = setTimeout(function(){runSlideShow(imageSet, current)}, imageSet[current].settings.duration);
+	} 
+	else {
+		if(timer){
+			clearTimeout(timer);
+			timer = null;
+		}
+		timer = setTimeout(function() {
+			endAnimation(imageSet[current-1]);
+		}, imageSet[current-1].settings.duration);
+	}
 }
