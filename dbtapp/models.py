359a5655347bd878f7fa803b95ebd2a539c80b24
wahjlen <micke_wahlstrom@hotmail.com>
2015-04-27 08:54:14 +0200

    Simple Model created

--- dbtapp/models.py
+++ dbtapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Video(models.Model):
+    video_name = models.CharField(max_length=100)
+    
+class Image(models.Model):
+    ImageVideoId = models.ForeignKey(Video)
+    image_url = models.CharField(max_length=10000)
+    
