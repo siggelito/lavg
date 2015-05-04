72bc7ddc65b500da2c0a1edabd53987f4f12f8ac
Hanna Soderstrom <hanna_soderstrom@live.se>
2015-05-04 16:24:03 +0200

    Riktigt indexsida och länkar i url och view

--- dbtapp/urls.py
+++ dbtapp/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from dbtapp import views
 
 urlpatterns = [
-     url(r'^$', views.index, name='index')
+     url(r'^$', views.index, name='index'),
+     url(r'^riktigIndex/$', views.riktigIndex, name='riktigIndex')
 ]
--- dbtapp/views.py
+++ dbtapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'dbtapp/index.html')
+
+def riktigIndex(request):
+    return render(request, 'dbtapp/riktigIndex.html')
