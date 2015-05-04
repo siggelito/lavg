d52404a86d2b2369a6e85c2ee9d8e4f0e2805895
wahjlen <micke_wahlstrom@hotmail.com>
2015-05-04 14:27:42 +0200

    update settings

--- dbt/settings.py
+++ dbt/settings.py
@@ -83,6 +83,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 #
-#STATIC_URL = '/static/'
-#MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
