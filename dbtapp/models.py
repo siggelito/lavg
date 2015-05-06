c8d9a2c3158f1379360fc3319b10585d52fef71f
Mattias <mattias.edin92@gmail.com>
2015-05-06 20:44:48 +0200

    Bugfix: Create new video

--- dbtapp/models.py
+++ dbtapp/models.py
@@ -7,15 +7,7 @@ class Video(models.Model):
     
 class Photo(models.Model):
     video = models.ForeignKey(Video, null=True, blank=True, default = None)
-    #image_url = models.CharField(max_length=10000)
-    photoFile = models.ImageField() #upload_to = 'images', default = 'images/None/no-img.jpg'
-    # def save(self, *args, **kwargs):
-    #     try:
-    #         this = Photo.objects.get(id=self.id)
-    #         if this.photoFile != self.photoFile:
-    #             this.photoFile.delete()
-    #     except: pass
-    #     super(Photo, self).save(*args, **kwargs)
+    photoFile = models.ImageField()
     
 # Receive the pre_delete signal and delete the file associated with the model instance.
 from django.db.models.signals import pre_delete
