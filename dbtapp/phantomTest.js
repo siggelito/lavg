602545ddeadd5c0ecf3b4c1ee47bc780681d2470
wahjlen <micke_wahlstrom@hotmail.com>
2015-05-13 13:06:19 +0200

    pjstest..

--- dbtapp/phantomTest.js
+++ dbtapp/phantomTest.js
@@ -1,5 +1,5 @@
 var page = require('webpage').create();
-page.viewportSize = { width: 640, height: 480 };
+//page.viewportSize = { width: 640, height: 480 };
 
 page.open('http://31.media.tumblr.com/323247f4fdf2cc14831bb7980bb432af/tumblr_njy5ihohLL1re3kvuo1_400.gif', 
 function () {
