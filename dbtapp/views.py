1894eda99136ee88ad6a99c89eff624b3c8f13c9
wahjlen <micke_wahlstrom@hotmail.com>
2015-05-13 12:13:43 +0200

    phantomjstest

--- dbtapp/views.py
+++ dbtapp/views.py
@@ -155,9 +155,9 @@ def phantomjs(request):
     
     #celery
     
-    phantomProcess = Popen([command, phantomjs_script], stdout=PIPE, stderr=PIPE)
+    phantomProcess = Popen([command, phantomjs_script], stdout=PIPE, stderr=STDOUT)
     
-    ffmpegProcess = Popen(['ffmpeg', '-y', '-c:v', 'png', '-f', 'image2pipe', '-r', '25', '-t', '1', '-i', '-c:v', 'libx264', '-pix_fmt', 'yuv420p', '-movflags', '+faststart', 'test.mp4'], stdin=phantomProcess.communicate(), stdout=PIPE, stderr=PIPE)
+    ffmpegProcess = Popen(['ffmpeg', '-y', '-c:v', 'png', '-f', 'image2pipe', '-r', '25', '-t', '1', '-i', '-c:v', 'libx264', '-pix_fmt', 'yuv420p', '-movflags', '+faststart', 'test.mp4'], stdin=phantomProcess, stdout=PIPE, stderr=STDOUT)
     
     #returnFile = File(open(fileName, 'r'))
     #response = HttpResponse(returnFile, mimetype='application/force-download')
