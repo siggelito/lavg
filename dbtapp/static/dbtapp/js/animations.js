43c202c91541fcdbf90cd23c6931d67a3782b31f
Mattias <mattias.edin92@gmail.com>
2015-05-13 09:58:36 +0200

    Fix broken commit

--- dbtapp/static/dbtapp/js/animations.js
+++ dbtapp/static/dbtapp/js/animations.js
@@ -1,3 +1,4 @@
+
 function simpleTransition(current, next, timeline) {
 /*	$(current.layers).fadeOut();
 	$(next.layers).fadeIn();*/
@@ -7,10 +8,11 @@ function simpleTransition(current, next, timeline) {
 		next.layers[0].setAttribute("style", "margin-left:" + 800 + "px");
 	}),null);
 	timeline.add(TweenLite.to($(next.layers[0]), 2, {marginLeft:"0px"}));
-	timeline.add(TweenLite.to($(current.layers[0]), 0, {display:"none"}));
-/*	timeline.add(TweenLite.delayedCall(0, function(){
+	timeline.add(TweenLite.to($(current.layers[0]), 0.1, {display:"none"}));
+	//timeline.add(TweenLite.delayedCall()
+	timeline.add(TweenLite.delayedCall(0, function(){
 		$(current).hide();
-  	}),null);*/
+  	}),null);
 }
 
 function startAnimation(layerContent) {
