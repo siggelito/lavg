d10e17af2543c61b15f97eafa5cf0f92e8021696
Karonsbo <karro.jonzen@hotmail.com>
2015-05-13 09:57:52 +0200

    Preview-sidan + startsida
    Ändrat i preview-sidan + stilmallen och effekter i startsidan!

--- dbtapp/static/dbtapp/js/landingPage.js
+++ dbtapp/static/dbtapp/js/landingPage.js
@@ -9,12 +9,47 @@ jQuery(document).ready(function($) {
   });
 });
 
+/* Scrolleffekt för "Skapa din video!"*/
+jQuery(document).ready(function($) {
 
+  $(".navbar-brand").click(function(event){   
+    event.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 300, 'swing');
+  });
+});
+
+
+/* TEST: Ändra menyn när man scrollar (med övergångar) */
+$(window).scroll(function() {
+
+  var scroll_pos = window.pageYOffset;
+  if(scroll_pos>=40) {
+      $( ".navbar" ).animate({
+        fontSize: '13px',
+                backgroundColor: '#2B2B2B'
+
+  }, 50, function() {
+    // Animation complete.
+  });
+}
+
+  else
+  {
+    $( ".navbar" ).animate({
+              fontSize: '16px',
+        backgroundColor: 'transparent'
+
+  }, 100, function() {
+    // Animation complete.
+  });
+  }
+}); 
 
 /*
 Change in navbar colour and fontsize when scrolling down.
+
 */
-$(window).scroll(function() {
+/*$(window).scroll(function() {
 
   var scroll_pos = window.pageYOffset;
   if(scroll_pos>=40) {
@@ -25,4 +60,4 @@ $(window).scroll(function() {
   {
     $(".navbar").css("background-color","transparent").css("font-size", "16px");
   }
-}); 
+}); */
