4c4ed3511770759c120a0a40595005f7c51d1006
wahjlen <micke_wahlstrom@hotmail.com>
2015-05-18 11:49:44 +0200

    pjstest

--- dbtapp/phantomTest.js
+++ dbtapp/phantomTest.js
@@ -3,9 +3,11 @@ var page = require('webpage').create();
 
 page.open('http://31.media.tumblr.com/323247f4fdf2cc14831bb7980bb432af/tumblr_njy5ihohLL1re3kvuo1_400.gif', 
 function () {
-	setInterval(function () {
-		page.render("/dev/stdout", {format: "png"});
-	}, 25);
+	setTimeout (function () {
+		setInterval(function () {
+			page.render("/dev/stdout", {format: "png"});
+		}, 25);
+	}, 5000);
 });
 
 /*
