1b4ab69641eb225de2392a60810c3117db302053
wahjlen <micke_wahlstrom@hotmail.com>
2015-05-22 10:38:23 +0200

    back to postgres

--- dbt/settings_server.py
+++ dbt/settings_server.py
@@ -2,17 +2,13 @@ from .settings import *
 
 DATABASES = {
 	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+		'ENGINE': 'django.db.backends.postgresql_psycopg2',
+		'NAME': 'dbt',
+		'HOST': 'localhost',
+		'USER': 'dbt',
+		'PASSWORD': 'dbt',
+		'ATOMIC_REQUESTS': True,
 	}
-# 	'default': {
-# 		'ENGINE': 'django.db.backends.postgresql_psycopg2',
-# 		'NAME': 'dbt',
-# 		'HOST': 'localhost',
-# 		'USER': 'dbt',
-# 		'PASSWORD': 'dbt',
-# 		'ATOMIC_REQUESTS': True,
-# 	}
 }
 BASE_DIR = '/var/www/dbt/project/'
 STATIC_ROOT = BASE_DIR + 'static'
