37d4a32de9c431b44cdc281c76e180dbad99de1f
wahjlen <micke_wahlstrom@hotmail.com>
2015-05-22 09:39:39 +0200

    fix postgres db problem

--- dbt/settings.py
+++ dbt/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'widget_tweaks',
-    'dbtapp',
+    #'widget_tweaks',
+    #'dbtapp',
 )
 
 MIDDLEWARE_CLASSES = (
