d7c4b423910208814349dd8c2fd8f152c6d5cbd3
wahjlen <micke_wahlstrom@hotmail.com>
2015-05-25 14:29:13 +0200

    back to postgres in production

--- dbt/settings_server.py
+++ dbt/settings_server.py
@@ -1,19 +1,20 @@
 from .settings import *
 
 DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-	}
 			
 # 	'default': {
-# 		'ENGINE': 'django.db.backends.postgresql_psycopg2',
-# 		'NAME': 'dbt',
-# 		'HOST': 'localhost',
-# 		'USER': 'dbt',
-# 		'PASSWORD': 'dbt',
-# 		'ATOMIC_REQUESTS': True,
+# 		'ENGINE': 'django.db.backends.sqlite3',
+# 		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 # 	}
+			
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql_psycopg2',
+		'NAME': 'dbt',
+		'HOST': 'localhost',
+		'USER': 'dbt',
+		'PASSWORD': 'dbt',
+		'ATOMIC_REQUESTS': True,
+	}
 }
 BASE_DIR = '/var/www/dbt/project/'
 STATIC_ROOT = BASE_DIR + 'static'
